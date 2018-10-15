4df9b547ca465c88aa4b3f8251595afc02ef4e5f
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2018-10-15 16:42:36 -0400

    Testing sending html instead of json to server

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', (req,res) => {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+    //res.sendFile(path.join(__dirname+'/dist/index.html'));   
+    res.status(200).send('<h1>Please... please...</h1>');
 });
 
 console.log('Console listening!');
