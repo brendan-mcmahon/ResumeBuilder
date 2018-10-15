774b88517775d93369261f22efea153eeabf9cc4
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2018-10-15 17:28:28 -0400

    file path was missing folder

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.listen(process.env.PORT || 8080);
 
 app.get('/*', (req,res) => {  
     //res.sendFile(path.join(__dirname+'/dist/index.html'));   
-    res.status(200).sendFile(path.join(__dirname+'/dist/index.html'));
+    res.status(200).sendFile(path.join(__dirname+'/dist/resume-app/index.html'));
 });
 
 console.log('Console listening!');
