b8fa9ff349f26aec1622494a0cd5766b16597431
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2018-10-15 22:57:30 -0400

    reverting server.js changes

--- server.js
+++ server.js
@@ -1,72 +1,18 @@
-// //Install express server    
-// const express = require('express');
-// const http = require('http');
-// const bodyParser = require('body-parser');
-
-// // Get our API routes
-// const api = require('./server/routes/api');
-// const path = require('path');   
-
-// const app = express();
-
-// // Parsers for POST data
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// // Serve only the static files form the dist directory    
-// app.use(express.static(path.join(__dirname + '/dist/resume-app')));
-
-// // Set our api routes
-// app.use('/api', api);
-
-// // Start the app by listening on the default Heroku port    
-// app.listen(process.env.PORT || 8080);
-
-// app.get('*', (req,res) => {  
-//     //res.sendFile(path.join(__dirname+'/dist/index.html'));   
-//     res.sendFile(path.join(__dirname+'/dist/resume-app/index.html'));
-// });
-
-// console.log('Console listening!');
-
-// Get dependencies
+//Install express server    
 const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
+const app = express();   
 
-// Get our API routes
-const api = require('./server/routes/api');
+const path = require('path');   
 
-const app = express();
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/dist'));
 
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// Start the app by listening on the default Heroku port    
+app.listen(process.env.PORT || 8080);
 
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Set our api routes
-app.use('/api', api);
-
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/resume-app/index.html'));
+app.get('/*', (req,res) => {  
+    //res.sendFile(path.join(__dirname+'/dist/index.html'));   
+    res.status(200).sendFile(path.join(__dirname+'/dist/resume-app/index.html'));
 });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+console.log('Console listening!');
