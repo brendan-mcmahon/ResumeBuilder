144aeb2871a583c994b7130e0899cdfaac236996
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2018-10-15 23:35:26 -0400

    change static served pages

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');   
 
 // Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/resume-app'));
 
 // Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
