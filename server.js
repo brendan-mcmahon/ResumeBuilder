c6cf1079a77eeb25b598088d924f3b445fdc7600
Brendan McMahon <brendanjeffreymcmahon@gmail.com>
2018-10-15 23:05:33 -0400

    removing the wildcard

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
 
-app.get('/*', (req,res) => {  
+app.get('/', (req,res) => {  
     //res.sendFile(path.join(__dirname+'/dist/index.html'));   
     res.status(200).sendFile(path.join(__dirname+'/dist/resume-app/index.html'));
 });
